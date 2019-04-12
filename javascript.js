69ba05748882fa65803b03278d804134f44e5c54
GustavoHenriqueSilva9 <gustavohenriquesilva124@gmail.com>
2019-04-12 16:31:19 -0300

    Altima atualização site pronto

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
 var contador = 0
+var ganhou = false;
 function jogada(indice) {
     let elemento = document.getElementsByClassName("bloco")[indice]
-    contador++
-    if (!elemento.querySelector('img')) {
+    
+    if (!elemento.querySelector('img') && (!ganhou)) {
         if (contador % 2 == 0) {
             elemento.innerHTML = '<img src="x.png">';
+            contador++
         }
         else {
             elemento.innerHTML = "<img src='o.png'>";
+            contador++
         }
     }
 
@@ -28,19 +31,22 @@ function puxar(elemento) {
     if ((elemento0.innerHTML == elemento1.innerHTML) && (elemento1.innerHTML == elemento2.innerHTML)) {
         if (elemento.innerHTML == '<img src="x.png">') {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O XIZINHO GANHOU<h1>"
+            ganhou=true;
         }
         else {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O BOLINHA GANHOU<h1>"
-
+            ganhou=true;
         }
     }
 
     if ((elemento3.innerHTML == elemento4.innerHTML) && (elemento4.innerHTML == elemento5.innerHTML)) {
         if (elemento.innerHTML == '<img src="x.png">') {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O XIZINHO GANHOU<h1>"
+            ganhou=true;
         }
         else {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O BOLINHA GANHOU<h1>"
+            ganhou=true;
 
         }
 
@@ -49,56 +55,69 @@ function puxar(elemento) {
     if ((elemento6.innerHTML == elemento7.innerHTML) && (elemento7.innerHTML == elemento8.innerHTML)) {
         if (elemento.innerHTML == '<img src="x.png">') {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O XIZINHO GANHOU<h1>"
+            ganhou=true;
         }
         else {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O BOLINHA GANHOU<h1>"
+            ganhou=true;
         }
     }
 
     if ((elemento0.innerHTML == elemento3.innerHTML) && (elemento3.innerHTML == elemento6.innerHTML)) {
         if (elemento.innerHTML == '<img src="x.png">') {
              document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O XIZINHO GANHOU<h1>"
+             ganhou=true;
         }
         else {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O BOLINHA GANHOU<h1>"
+            ganhou=true;
         }
     }
 
     if (elemento1.innerHTML == elemento4.innerHTML && elemento4.innerHTML == elemento7.innerHTML) {
         if (elemento.innerHTML == '<img src="x.png">') {
              document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O XIZINHO GANHOU<h1>"
+             ganhou=true;
         }
         else {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O BOLINHA GANHOU<h1>"
+            ganhou=true;
         }
     }
     if (elemento2.innerHTML == elemento5.innerHTML && elemento5.innerHTML == elemento8.innerHTML) {
         if (elemento.innerHTML == '<img src="x.png">') {
              document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O XIZINHO GANHOU<h1>"
+             ganhou=true;
         }
         else {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O BOLINHA GANHOU<h1>"
+            ganhou=true;
         }
     }
     if (elemento0.innerHTML == elemento4.innerHTML && elemento4.innerHTML == elemento8.innerHTML) {
         if (elemento.innerHTML == '<img src="x.png">') {
              document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O XIZINHO GANHOU<h1>"
+             ganhou=true;
         }
         else {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O BOLINHA GANHOU<h1>"
+            ganhou=true;
         }
     }
     if (elemento2.innerHTML == elemento4.innerHTML && elemento4.innerHTML == elemento6.innerHTML) {
         if (elemento.innerHTML == '<img src="x.png">') {
              document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O XIZINHO GANHOU<h1>"
+             ganhou=true;
         }
         else {
             document.getElementById("ganhou").innerHTML = "<h1>O JOGADOR QUE UTILIZOU O BOLINHA GANHOU<h1>"
+            ganhou=true;
         }
 
     }
     if ( contador == 9){
         
         document.getElementById("ganhou").innerHTML = "<h1>DEU VELHA!!!<h1>"
+        ganhou=true;
     }
 }
